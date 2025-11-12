27ac0674b1575af534ae794dea417a5164033fb3
Andre-Roddrigues <lariotyler6@gmail.com>
2025-11-12 12:40:28 +0200

    fix: fix

--- components/ui/text-effect.tsx
+++ components/ui/text-effect.tsx
@@ -6,7 +6,7 @@ import {
   TargetAndTransition,
   Variants,
 } from 'framer-motion';
-import React from 'react';
+import React, { JSX } from 'react';
 
 type PresetType = 'blur' | 'shake' | 'scale' | 'fade' | 'slide';
 
