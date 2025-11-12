5111840b2386adc61b204c3f98a5eddbc4025f3c
Andre-Roddrigues <lariotyler6@gmail.com>
2025-11-12 12:59:04 +0200

    style: navbar

--- components/Landing/Navbar/navbar.tsx
+++ components/Landing/Navbar/navbar.tsx
@@ -136,18 +136,6 @@ const Navbar = () => {
             </div>
           )}
 
-          {/* Botão de dark mode */}
-          {/* <button
-            onClick={toggleDarkMode}
-            className="p-2 rounded-full bg-gray-100 dark:bg-gray-800 hover:scale-105 transition"
-          >
-            {darkMode ? (
-              <Sun className="w-5 h-5 text-yellow-400" />
-            ) : (
-              <Moon className="w-5 h-5 text-gray-700 dark:text-gray-200" />
-            )}
-          </button> */}
-
           {/* Botão menu mobile */}
           <button
             onClick={() => setIsMobileOpen(true)}
