786bdf795b485bb31f9873473bd30df682f723d2
Andre-Roddrigues <lariotyler6@gmail.com>
2025-11-13 14:57:12 +0200

    feat: SEO

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,9 @@ import CTAGraduacao from "../components/Landing/Graduacao/CTAGraduacao";
 import GraduacaoPage from "../components/Landing/Graduacao/GraduacaoPage";
 
 export const metadata: Metadata = {
-  title: "",
-  description: "Programa de Melhoria de Empregabilidade e Trabalho que fortalece competências, promove inserção profissional e apoia carreiras sustentáveis.",
+  title: "Graduação Unitec",
+  description: "Guia para preencher seu currículo na Unitec, incluindo informações pessoais, formação acadêmica e experiências relevantes para a graduação.",
 };
-
 export default function Home() {
 
   return (
